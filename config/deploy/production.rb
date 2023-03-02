1916617495845648722ec91c19a2a21fcb5a27d8
ashishkpaul <ashish.kpaul@gmail.com>
2023-03-02 17:14:20 +0530

    Configure env path in deploy.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-server '140.238.230.137', user: 'deploy', roles: %w{app db web}
+server '140.238.230.137', user: 'deploy', roles: %w{app db web}, primary: true ?
 set :branch, 'main'
 
 # role-based syntax
